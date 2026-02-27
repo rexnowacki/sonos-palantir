785a1ab60881f74e1ca05e46a95dffeeb64a5a33
Rex Nowacki <nowacki.chris@gmail.com>
2026-02-27 16:52:04 -0700

    chore: add .gitignore and untrack .venv

--- daemon/.venv/lib/python3.12/site-packages/soco/music_services/__init__.py
+++
@@ -1,8 +0,0 @@
-"""This package provides the MusicService class and related functionality,
-which allows access to the various third party music services which can be used
-with Sonos."""
-
-from .music_service import MusicService
-from .accounts import Account
-
-__all__ = ["MusicService", "Account"]
